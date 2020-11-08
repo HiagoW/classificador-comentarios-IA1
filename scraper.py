bbec167c0cd83d3f376d05eb8a17513ee3cb5128
Hiago W Petris <hiago.petris@gmail.com>
2020-11-08 11:11:29 -0300

    buscando avaliações

--- scraper.py
+++ scraper.py
@@ -1,43 +1,87 @@
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-from datetime import datetime
+import csv
 import time
 import re
-import logging
-import traceback
+import io
 
-options = Options()
-driver = webdriver.Chrome()
+urls = \
+    [\
+    "https://www.google.com/maps/place/UPA+24h+Boa+Vista/@-25.3857065,-49.2350658,17z/data=!4m7!3m6!1s0x94dce66f6ff5ece5:0x1b92d4f7158c1e76!8m2!3d-25.3857065!4d-49.2328771!9m1!1b1",\
+    "https://www.google.com/maps/place/UPA+Maracan%C3%A3/@-25.3614534,-49.1915863,17z/data=!4m7!3m6!1s0x0:0x8954c9bb8097d648!8m2!3d-25.3614534!4d-49.1893976!9m1!1b1"\
+    ]
 
-url = "https://www.google.com/maps/place/UPA+24h+Boa+Vista/@-25.3857065,-49.2350658,17z/data=!3m1!4b1!4m5!3m4!1s0x94dce66f6ff5ece5:0x1b92d4f7158c1e76!8m2!3d-25.3857065!4d-49.2328771"
+reviews = []
 
-driver.get(url)
+for url in urls:
 
-wait = WebDriverWait(driver, 10)
+    driver = webdriver.Chrome()
+    driver.get(url)
 
-response = BeautifulSoup(driver.page_source, 'html.parser')
-rlist = response.find_all('div', class_='section-review-content')
+    wait = WebDriverWait(driver, 10)
 
-for r in rlist:
-    try:
-        review_text = r.find('span', class_='section-review-text').text
-    except Exception:
-        review_text = None
+    time.sleep(5)
+    
+    # Pega div dos comentarios
+    pane = driver.find_elements_by_xpath(
+                                        '//div[@class=\'section-layout section-scrollbox scrollable-y scrollable-show\']')[0]
+    # Pega altura do scroll
+    last_height = pane.get_attribute("scrollHeight")
 
-    print(review_text)
-    print(10*'-')
+    while True:
+        # Faz o scroll até o último review
+        last_review = driver.find_elements_by_xpath(
+                                        '//div[@class=\'section-review ripple-container GLOBAL__gm2-body-2\']')[-1]
+        driver.execute_script('arguments[0].scrollIntoView(true);', last_review)
 
-# Scroll não funcionando
-scrollable_div = driver.find_element_by_css_selector(
- 'div.section-layout.section-scrollbox.scrollable-y.scrollable-show'
-                     )
-driver.execute_script(
-               'arguments[0].scrollTop = arguments[0].scrollHeight', 
-                scrollable_div
-               )
+        time.sleep(3)
+
+        # Nova altura do scroll
+        new_height = pane.get_attribute("scrollHeight")
+
+        # Pega ultimo comentario
+        response = BeautifulSoup(driver.page_source, 'html.parser')
+        ultimaDivComentario = response.find_all('div', class_='section-review-content')[-1]
+
+        ''' Se o ultimo comentario for vazio, pode dar break pq começam 
+        a aparecer apenas avaliações sem comentários '''
+        
+        try:
+            review_text = ultimaDivComentario.find('span', class_='section-review-text').text
+            if review_text.strip() == "":
+                break
+        except Exception:
+            break
+        
+        ''' Se a altura do scroll for igual a anterior, não tem mais 
+        reviews para carregar '''
+
+        if new_height == last_height:
+            break
+
+        last_height = new_height
+
+    response = BeautifulSoup(driver.page_source, 'html.parser')
+    rlist = response.find_all('div', class_='section-review-content')
+
+    for r in rlist:
+        
+        # Salva a qtd de estrelas
+        rating = r.find('span', class_='section-review-stars')['aria-label']
+        rating = re.sub("[^0-9]", "", rating)
+        
+        # Salva comentario se não for
+        try:
+            review_text = r.find('span', class_='section-review-text').text
+            if review_text.strip() != "":
+                reviews.append((rating,review_text))
+        except Exception:
+            review_text = None
+
+with io.open('output.csv','w',newline='\n',encoding="utf-8") as result_file:
+    csv_out=csv.writer(result_file, delimiter=';')
+    csv_out.writerow(['classificacao','texto'])
+    for row in reviews:
+        csv_out.writerow(row)
